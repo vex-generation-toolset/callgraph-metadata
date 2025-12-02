841d308d4b23dbdcc125d73f456ed5267413acf9
Piotr P. Karwasz <piotr@github.copernik.eu>
2025-12-02 20:37:55 +0100

    Improve verbosity of run-workflows-for-sbom.sh
    This change does not show the workflows skipped due to missing source location data.

--- scripts/run-workflows-for-sbom.sh
+++ scripts/run-workflows-for-sbom.sh
@@ -1,9 +1,43 @@
 #!/bin/bash
 
-SBOM="$1"
-if [ -z "$SBOM" ]; then
-    echo "Usage: $0 <path-to-sbom>"
-    exit 1
+function log_debug() {
+    if [ "$DEBUG" -eq 1 ]; then
+        echo "[DEBUG] $1" >&2
+    fi
+}
+
+function log_info() {
+    echo "[INFO] $1" >&2
+}
+
+SBOM=""
+SHOW_USAGE=0
+DEBUG=0
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -d) DEBUG=1; shift ;;
+        -h|--help) SHOW_USAGE=1; break ;;
+        --) shift; break ;;
+        -*) echo "Unknown option: $1" >&2; exit 1 ;;
+        *) if [ -z "$SBOM" ]; then
+               SBOM="$1";
+               shift;
+           else
+               echo "Only one path-to-sbom allowed" >&2;
+               exit 1;
+          fi ;;
+    esac
+done
+
+if [[ -z "$SBOM" || $SHOW_USAGE -eq 1 ]]; then
+    echo "Usage: $0 <options> <path-to-sbom>"
+    echo "Options:"
+    echo "  -d  Debug mode"
+    if [ "$SHOW_USAGE" -eq 1 ]; then
+        exit 0
+    else
+        exit 1
+    fi
 fi
 
 jq -r '.components[].purl' "$SBOM" | while read -r purl; do
@@ -20,8 +54,9 @@ jq -r '.components[].purl' "$SBOM" | while read -r purl; do
     fi
     location_file="callgraphs/$groupId/$artifactId/location.json"
     if [ -f "$location_file" ]; then
-        gh workflow run generate-call-graph.yaml -f artifact="$groupId:$artifactId:$version"
+        gh workflow run generate-call-graph.yaml -f artifact="$groupId:$artifactId:$version" >&2
+        log_info "Triggered workflow for: $groupId:$artifactId:$version"
     else
-        echo "Missing location file for: $groupId:$artifactId:$version" >&2
+        log_debug "Missing location file for: $groupId:$artifactId:$version"
     fi
 done
